a31c243a62fdeb649bfa3938c1effed4d7520c62
Ida-Sofie Karlsson <ida.sofie.karlsson@gmail.com>
2025-04-21 05:44:25 +0200

    assignment done

--- script.js
+++ script.js
@@ -41,7 +41,58 @@ let score = 0;
 //Initializing time
 let time = 10;
 
+//neccessary functions
+function addWordToDOM() {
+  let randomNr = Math.floor(Math.random() * 19);
+  randomWord = words[randomNr];
+  return randomWord;
+};
+
+function updateScore() {
+  score++;
+  return score;
+};
+
+function gameOver() {
+    endgameEl.style.display="unset";
+    //display the end-game-container 
+}
+
+
+const myInterval = setInterval(updateTime, 1000);
+
+function updateTime() {
+  time = time - 1;
+  if (time <= 0) {
+    clearInterval(myInterval);
+    gameOver();
+    return;
+  }
+  timeEl.textContent = time;
+};
+
+//give a first word to type and start timer
+randomWord = addWordToDOM();
+word.textContent = randomWord;
+
+updateTime();
+
+const input = document.querySelector("input");
+
+//if you type correctly, then: 
+input.addEventListener("input", function(e) {
+  if (this.value === randomWord) {
+    updateScore();
+    scoreEl.textContent = score;
+    time = time + 5;
+    
+    word.textContent = addWordToDOM();
+    this.value = "";
+    }
+});
 
 /*egna antackningar
-eventlisteners.mp4 10:00 - 11:02 "fånga" vad som skrivs i rutan, efter slagit enter
+eventlisteners.mp4 
+ca 7:00 fånga event (e)
+10:00 - 11:02 "fånga" vad som skrivs i rutan, efter slagit enter
 */
--- test.js
+++
@@ -1,8 +0,0 @@
-let userName = prompt("what's your name? ");
-const welcomeMsg = document.getElementById("msg");
-
-if (userName === "") {
-    welcomeMsg.textContent = `Hello user!`;
-} else {
-    welcomeMsg.textContent = `Hello ${userName}`;
-}
